d68babd8230773c76988819cb1d1c894d4ca15cd
zep1994 <thomasmatlockbba@gmail.com>
2022-08-04 10:06:10 -0500

    Move

---
+++ move.py
@@ -0,0 +1,10 @@
+import time
+ 
+# a module which has functions related to time.
+# It can be installed using cmd command:
+# pip install time, in the same way as pyautogui.
+import pyautogui
+
+pyautogui.moveTo(100, 100, duration = 14400)
+pyautogui.moveTo(1000, 1000, duration = 14400)
+
